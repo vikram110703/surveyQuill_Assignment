4c5a55f892098abfa3efe7ff6ae9e535c871685f
vikram110703 <sainivks4321@gmail.com>
2024-12-07 14:24:27 +0530

    base url updated

--- libs/constant.js
+++ libs/constant.js
@@ -1,2 +1,3 @@
- const BASE_URL='http://localhost:3000';
+//  const BASE_URL='survey-quill-assignment.vercel.app'||'http://localhost:3000';
+ const BASE_URL='survey-quill-assignment.vercel.app';
  export default BASE_URL;
